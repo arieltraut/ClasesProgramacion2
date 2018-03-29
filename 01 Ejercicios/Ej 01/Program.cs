98698a9029c0a14c8c1161d77ad0311497264f5d
arieltraut <arieltraut@gmail.com>
2018-03-29 20:27:44 -0300

    cambios de nombre

--- 01 Ejercicios/Ej 01/Program.cs
+++ 01 Ejercicios/Ej 01/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Ej_1
+namespace Ej_01
 {
     class Program
     {
---
+++ 01 Ejercicios/Ej 02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double aux = 0, cuadrado = 0, cubo = 0;
+
+            Console.WriteLine("Ingrese un numero");
+            if (double.TryParse(Console.ReadLine(), out aux)) //preguntar si es true o false
+            {
+                if (aux > 0)
+                {
+                    cuadrado = Math.Pow(aux, 2);
+                    cubo = Math.Pow(aux, 3);
+                    Console.WriteLine("El cuadrado es {0}", cuadrado);
+                    Console.WriteLine("El cubo es {0}", cubo);
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("ERROR. ¡Reingresar número!");
+                }
+            }
+
+        }
+    }
+}
