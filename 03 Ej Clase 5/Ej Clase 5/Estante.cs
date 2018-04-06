fab83fa73908c3e4a894b033a49369270f82c2cb
arieltraut <arieltraut@gmail.com>
2018-04-06 15:42:19 -0300

    ej clase 5

---
+++ 03 Ej Clase 5/Ej Clase 5/Estante.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej_Clase_5
+{
+    class Estante
+    {
+        private int ubicacionEstante;
+        private Producto[] productos;
+
+        #region Constructores
+
+        /// <summary>
+        /// Inicializo un nuevo estante, inicializando la lista según una capacidad.
+        /// </summary>
+        /// <param name="capacidad">Cantidad de productos que podrá contener el estante.</param>
+        private Estante(int capacidad)
+        {
+            this.productos = new Producto[capacidad];
+        }
+
+        /// <summary>
+        /// Inicializo un nuevo Estante, asignando los atributos capacidad y ubicación
+        /// </summary>
+        /// <param name="capacidad">Cantidad de productos que podrá contener el estante.</param>
+        /// <param name="ubicacion">Código de ubicación del Estante</param>
+        public Estante(int capacidad, int ubicacion) : this(capacidad)
+        {
+            this.ubicacionEstante = ubicacion;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Publico la lista de Productos
+        /// </summary>
+        /// <returns></returns>
+        public Producto[] GetProductos ()
+        {
+            return this.productos;
+        }
+
+        /// <summary>
+        /// Muestro los datos del estante elegido
+        /// </summary>
+        /// <param name="e"></param>
+        /// <returns></returns>
+        public static string MostrarEstante (Estante e)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("La ubicacion del estante es: " + e.ubicacionEstante);
+            Producto[] p = e.GetProductos();
+            for (int i = 0; i < p.Length; i++)
+            {
+                sb.AppendLine(Producto.MostrarProducto(p[i]));
+            }
+            /* Con forech seria asi:
+            foreach (Producto p in e.productos)
+            {
+                sb.AppendLine(Producto.MostrarProducto(p));
+            }*/
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Analizo si el Producto está en el Estante
+        /// </summary>
+        /// <param name="e"></param>
+        /// <param name="p"></param>
+        /// <returns>True si el Producto está en el Estante</returns>
+        public static bool operator ==(Estante e, Producto p)
+        {
+            foreach (Producto aux in e.productos)
+            {
+                // Valido que la posición del Array no sea null
+                if (!object.ReferenceEquals(aux, null))
+                {  
+                    if (aux == p) // Utilizo la sobrecarga del == de Producto
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Analizo si el Producto NO está en el Estante
+        /// </summary>
+        /// <param name="e"></param>
+        /// <param name="p"></param>
+        /// <returns>True si el Producto NO está en el Estante</returns>
+        public static bool operator !=(Estante e, Producto p)
+        {
+            return !(e == p); // Al negar la sobrecarga == estamos haciendo la inversa
+        }
+
+
+        /// <summary>
+        /// Agrego un Producto al Estante, si hay lugar y no hay uno igual ya cargado
+        /// </summary>
+        /// <param name="e"></param>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public static bool operator +(Estante e, Producto p)
+        {
+            // Si el Estante no contiene el Producto...
+            if (e != p)
+            {
+                // Recorro la lista de productos del estante...
+                for (int i = 0; i < e.productos.Length; i++)
+                {
+                    // Busco un espacio vacio
+                    if (object.ReferenceEquals(e.productos[i], null))
+                    {
+                        e.productos[i] = p;
+                        // Si agregué, rompo el lazo para no volver a agregarlo.
+                        return true;
+                    }
+                }
+            }
+            // Si no salió hasta acá es porque no agregó el ítem
+            return false;
+        }
+
+        public static Estante operator -(Estante e, Producto p)
+        {
+            if (e == p)
+            {
+                for (int i = 0; i < e.productos.Length; i++)
+                {
+                    if (object.ReferenceEquals(e.productos[i], p))
+                    {
+                        e.productos[i] = null;
+                        break;
+                    }
+                }
+            }
+            return e;
+        }       
+    }
+}
---
+++ 03 Ej Clase 5/Ej Clase 5/Producto.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej_Clase_5
+{
+    class Producto
+    {
+        protected string codigoDeBarra;
+        protected string marca;
+        protected float precio;
+
+        public Producto(string marca, string codigo, float precio)
+        {
+            this.codigoDeBarra = codigo;
+            this.marca = marca;
+            this.precio = precio;
+        }
+
+        public string GetMarca()
+        {
+            return this.marca;
+        }
+
+        public float GetPrecio()
+        {
+            return this.precio;
+        }
+
+        public static string MostrarProducto(Producto p)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("Codigo de barra: " + p.codigoDeBarra);
+            sb.AppendLine("Marca: " + p.marca);
+            sb.AppendLine("Precio: " + p.precio);
+            sb.AppendLine("-------------");
+            return sb.ToString();
+        }
+
+        public static explicit operator string(Producto p)
+        {
+            return p.codigoDeBarra;
+        }
+
+        public static bool operator ==(Producto p1, Producto p2)
+        {
+            bool retorno = false;
+            if (!(object.ReferenceEquals(p1, null)) && !(object.ReferenceEquals(p2, null)))
+            {
+                if (p1.marca == p2.marca && p1.codigoDeBarra == p2.codigoDeBarra)
+                    retorno = true;
+                else
+                    retorno = false;
+            }
+            return retorno;
+        }
+
+        public static bool operator !=(Producto p1, Producto p2)
+        {
+            if (p1.marca == p2.marca && p1.codigoDeBarra == p2.codigoDeBarra)
+                return false;
+            else
+                return true;
+        }
+
+        public static bool operator ==(Producto p, string marca)
+        {
+            if (p.marca == marca)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator !=(Producto p, string marca)
+        {
+            if (p.marca == marca)
+                return false;
+            else
+                return true;
+        }
+
+
+
+    }
+}
