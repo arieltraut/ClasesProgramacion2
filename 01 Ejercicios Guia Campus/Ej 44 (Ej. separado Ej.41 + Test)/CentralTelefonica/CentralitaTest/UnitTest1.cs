b3ec2090e216b85a62829aa1942da9c7106f102a
arieltraut <arieltraut@gmail.com>
2018-05-19 11:50:58 -0300

    ej 47

---
+++ 01 Ejercicios Guia Campus/Ej 44 (Ej. separado Ej.41 + Test)/CentralTelefonica/CentralitaTest/UnitTest1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CentralitaHerencia;
+using System.Collections.Generic;
+
+namespace CentralitaTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void a()
+        {
+
+            Centralita centralita = new Centralita("CentralTest");
+            Assert.IsNotNull(centralita.Llamadas);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CentralitaException))]    
+        public void b()
+        {
+            Centralita centralita = new Centralita("CentralTest");
+
+            
+            Local l1 = new Local("Lanús", 90, "San Rafael", 3.99f);
+            Local l2 = new Local("Lanús", 45, "San Rafael", 1.99f);
+
+            centralita += l1;
+            centralita += l2;
+            
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(CentralitaException))]
+        public void c()
+        {
+            Centralita centralita = new Centralita("CentralTest");
+
+
+            Provincial l1 = new Provincial("Morón", Provincial.Franja.Franja_1, 21, "Bernal");
+            Provincial l2 = new Provincial("Morón", Provincial.Franja.Franja_1, 21, "Bernal");
+
+            centralita += l1;
+            centralita += l2;
+
+        }
+
+        [TestMethod]
+        public void d()
+        {
+            Centralita centralita = new Centralita("CentralTest");
+
+            Local l1 = new Local("Lanús", 90, "San Rafael", 3.99f);
+            Local l2 = new Local("Lanús", 45, "San Rafael", 1.99f);
+            Provincial p1 = new Provincial("Lanús", Provincial.Franja.Franja_1, 21, "San Rafael");
+            Provincial p2 = new Provincial("Lanús", Provincial.Franja.Franja_1, 21, "San Rafael");
+
+            
+            Assert.IsTrue(l1 == l2);
+            Assert.IsTrue(p1 == p2);
+            Assert.IsFalse(l1 == p1);
+            Assert.IsFalse(l1 == p2);
+            Assert.IsFalse(l2 == p1);
+            Assert.IsFalse(l2 == p2);
+
+        }
+    }
+}
---
+++ 01 Ejercicios Guia Campus/Ej 47 (Ej. separado Generics)/Ej 47/Entidades/Torneo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Torneo<T> where T : Equipo
+    {
+        private List<T> equipos;
+        private T a;
+        private string nombre;
+
+
+        public static bool operator ==(Torneo<T> torneo, T equipo)
+        {
+            foreach (T aux in torneo.equipos)
+            {
+                if (Object.Equals(aux,equipo))
+                    return true;
+            }    
+            return false;
+        }
+
+        public static bool operator !=(Torneo<T> torneo, T equipo)
+        {
+            return !(torneo == equipo);
+        }
+
+        public static Torneo<T> operator +(Torneo<T> torneo, T equipo)
+        {
+            if(torneo == equipo)
+                return torneo;
+
+            torneo.equipos.Add(equipo);
+            return torneo;
+        }
+
+        public string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("{0}\t{1}\n", this.a.ToString(), this.nombre);
+            foreach (T equipo in this.equipos)
+            {
+                sb.AppendLine(equipo.ToString());
+            }
+
+            return sb.ToString();
+        }
+
+        private string CalcularPartido<T, T>(T a, T b) where T : Equipo
+        {
+            Random r = new Random();
+            //r.Next(
+            return String.Format("{0}{1} - {2}{3}" ;
+        }
+    }
+}
