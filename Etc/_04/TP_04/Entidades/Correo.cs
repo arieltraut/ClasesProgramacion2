615c13fcaef07f838b48478932b8d0f66206110c
arieltraut <arieltraut@gmail.com>
2018-06-16 14:04:29 -0300

    16

---
+++ Etc/_04/TP_04/Entidades/Correo.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Entidades
+{
+    public class Correo : IMostrar<List<Paquete>>
+    {
+        private List<Thread> mockPaquetes;
+        private List<Paquete> paquetes;
+
+        public Correo()
+        {
+ 
+        }
+        
+        #region Propiedades
+        public List<Paquete> Paquetes
+        {
+            get { return this.paquetes; }
+            set { this.paquetes = value; }
+        }
+        #endregion
+
+
+        #region Metodos
+        public void FinEntregas()
+        {
+            foreach (Thread aux in this.mockPaquetes)
+            {
+                if (aux.IsAlive)
+                    aux.Abort();
+            }          
+            //Environment.Exit(Environment.ExitCode);
+        }
+                
+        public string MostrarDatos(IMostrar<List<Paquete>> elementos)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach(Paquete aux in (List<Paquete>)elementos)
+            {
+                sb.AppendFormat("{0} para {1} ({2})", aux.TrackingID, aux.DireccionEntrega,
+                aux.Estado.ToString()); //ver estado
+            }
+            return sb.ToString();
+        }
+        #endregion
+
+
+        #region Operadores
+        public static Correo operator +(Correo c, Paquete p)
+        {
+            foreach (Paquete aux in c.Paquetes)
+            {
+                if (aux == p)
+                    throw new TrackingIdRepetidoException("Paquete repetido");
+            }
+            c.Paquetes.Add(p);
+            Thread hiloMock = new Thread(p.MockCicloDeVida);
+            c.mockPaquetes.Add(hiloMock);            
+            hiloMock.Start();
+            return c;
+        }       
+        #endregion
+
+    }
+}
---
+++ Etc/_04/TP_04/Entidades/Paquete.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Entidades
+{
+    public class Paquete : IMostrar<Paquete>
+    {
+        private string direccionEntrega;
+        //EEstado estado;
+        private string trackingID;
+
+        #region Constructor
+        public Paquete(string direccionEntrega, string trackingID)
+        {
+            this.direccionEntrega = direccionEntrega;
+            this.trackingID = trackingID;
+        }
+        #endregion
+
+        
+        #region Propiedades (falta estado)
+        public string DireccionEntrega
+        {
+            get { return this.direccionEntrega; }
+            set { this.direccionEntrega = value; }
+        }
+
+        //public EEstado Estado
+        //{
+        //    get { return this.estado; }
+        //    set { this.estado = value; }
+        //}
+
+        public string TrackingID
+        {
+            get { return this.trackingID; }
+            set { this.trackingID = value; }
+        }
+        #endregion
+
+        #region Metodos
+        public string MostrarDatos(IMostrar<Paquete> elemento)
+        {
+            return String.Format("{0} para {1}", elemento.trackingID, elemento.direccionEntrega);
+        }
+
+        public void MockCicloDeVida()
+        {
+            Thread.Sleep(10000);
+
+        }
+
+
+
+        #endregion
+
+        #region Operators & Override (ver ToString)
+        public override string ToString(IMostrar<Paquete> elemento) //ver
+        {
+            return elemento.MostrarDatos(elemento);
+        }
+
+        public static bool operator ==(Paquete p1, Paquete p2)
+        {
+            if (ReferenceEquals(p1, null) || ReferenceEquals(p2, null))
+                return false;
+            return (p1.TrackingID == p2.TrackingID);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return this == (Paquete)obj;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+        #endregion
+
+
+    }
+}
